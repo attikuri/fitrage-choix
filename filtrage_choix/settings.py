f8ea80e97eec0f682f65569dc2dde05ad6f0a1ca
Khadidja <gausse.euclide@gmail.com>
2026-07-14 18:38:33 +0200

    render / vercel issues resolved

--- filtrage_choix/settings.py
+++ filtrage_choix/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["filtrage-choix.onrender.com", "127.0.0.1"]
 CORS_ALLOWED_ORIGINS = [
-    "https://filtrage-choix.vercel.app/",
+    "https://filtrage-choix.vercel.app",
 ]
 
 
